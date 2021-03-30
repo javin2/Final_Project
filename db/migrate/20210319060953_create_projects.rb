db62c292bade5daa9705c6798db03dca430f8c86
Nazira Usenkanova <nusenkanova@gmail.com>
2021-03-29 18:53:39 -0700

    fixed gemfile and migration script

--- db/migrate/20210319060953_create_projects.rb
+++ db/migrate/20210319060953_create_projects.rb
@@ -4,8 +4,8 @@ class CreateProjects < ActiveRecord::Migration[6.1]
       t.date :projected_completion
       t.string :title
       t.string :description
-      t.int :percent_complete
-      t.int :balance
+      t.integer :percent_complete
+      t.integer :balance
       t.belongs_to :client, null: false, foreign_key: true
 
       t.timestamps
---
+++ db/schema.rb
@@ -0,0 +1,56 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_03_19_060953) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "company_name"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.string "zipcode"
+    t.string "phone"
+    t.string "email"
+    t.integer "rate"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "developers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone"
+    t.string "company_name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.date "projected_completion"
+    t.string "title"
+    t.string "description"
+    t.integer "percent_complete"
+    t.integer "balance"
+    t.bigint "client_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["client_id"], name: "index_projects_on_client_id"
+  end
+
+  add_foreign_key "projects", "clients"
+end
